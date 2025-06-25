b9dc6d505b60b5d5168d29cd90d473e4b355f3a4
Ephraiem Sarabamoun <ephraiemsam16@gmail.com>
2025-06-25 08:02:05 -0400

    Started

---
+++ resources.py
@@ -0,0 +1,11 @@
+
+class resources:
+    WOOD = "wood"
+    BRICK = "brick"
+    SHEEP = "sheep"
+    WHEAT = "wheat"
+    ORE = "ore"
+
+    @staticmethod
+    def all_resources():
+        return [resources.WOOD, resources.BRICK, resources.SHEEP, resources.WHEAT, resources.ORE]
